33172ce4ebab684a4067acdaa3b4aae8cdba1925
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-25 12:12:36 -0600

    Header TSX: mobile menu btn

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,3 +1,12 @@
+import styles from "./header.module.scss";
+import mobileMenuBtn from "../../../assets/shared/mobileMenuBtn.png";
+
 export default function Header() {
-  return <></>;
+  return (
+    <header className={styles.header}>
+      <button className={styles.menuBtn} aria-label="Toggle mobile menu">
+        <img className={styles.menuIcon} alt="" src={mobileMenuBtn} />
+      </button>
+    </header>
+  );
 }
