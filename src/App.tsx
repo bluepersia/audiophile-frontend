cac94204bb137e368d4bd9f6442f606e02dd3cd3
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-25 13:23:58 -0600

    Tanstack react query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/shared/Layout/Layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,1 +1,12 @@
-export default function Hero() {}
+import clsx from "clsx";
+import styles from "./hero.module.scss";
+import getHeroData from "./services";
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <span className={clsx(styles.newProduct, "overline")}>New Product</span>
+      <h1 className={styles.title}></h1>
+    </section>
+  );
+}
