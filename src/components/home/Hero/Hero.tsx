bf13e2e8f3592d858c05c08f913603a753f65499
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-25 14:34:15 -0600

    Hero link fix

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import styles from "./hero.module.scss";
 import getHeroData from "./services";
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 
 type CSSVariables = {
   [key: `--${string}`]: string | undefined;
@@ -24,7 +25,12 @@ export default function Hero() {
         <span className={clsx(styles.newProduct, "overline")}>New Product</span>
         <h1 className={clsx(styles.title, "h3")}>{data?.title || ""}</h1>
         <p className={clsx(styles.desc, "body")}>{data?.desc || ""}</p>
-        <button className={clsx(styles.button, "button")}>See Product</button>
+        <Link
+          to={`/products/${data?.produtId}`}
+          className={clsx(styles.button, "button")}
+        >
+          See Product
+        </Link>
       </div>
     </section>
   );
--- src/components/home/Hero/services.ts
+++ src/components/home/Hero/services.ts
@@ -4,6 +4,7 @@ const heroData = {
   imgDesktop: "/assets/home/desktop/image-hero.jpg",
   title: "XX99 Mark II Headphones",
   desc: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+  produtId: 0,
 };
 
 export default async function getHeroData() {
