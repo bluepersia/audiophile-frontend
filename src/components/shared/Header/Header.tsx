fbae5607b601d292c4435c1fb03eb046b8d66005
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-25 12:21:52 -0600

    Header TSX: cart btn

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styles from "./header.module.scss";
 import mobileMenuBtn from "../../../assets/shared/mobileMenuBtn.png";
 import logo from "../../../assets/shared/logo.png";
+import cartIcon from "../../../assets/shared/cartIcon.png";
 import Nav from "../Nav/Nav";
 export default function Header() {
   return (
@@ -10,6 +11,9 @@ export default function Header() {
       </button>
       <img className={styles.logo} alt="Audiophile" src={logo} />
       <Nav />
+      <button className={styles.cartBtn} aria-label="Toggle cart">
+        <img className={styles.cartIcon} alt="" src={cartIcon} />
+      </button>
     </header>
   );
 }
