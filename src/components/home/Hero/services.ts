01e06f761a34356c9a920d8262df3341c5447c17
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-25 13:19:35 -0600

    Hero data

---
+++ src/components/home/Hero/services.ts
@@ -0,0 +1,11 @@
+const heroData = {
+  imgMobile: "/assets/home/mobile/image-hero.jpg",
+  imgTablet: "/assets/home/tablet/image-hero.jpg",
+  imgDesktop: "/assets/home/desktop/image-hero.jpg",
+  title: "XX99 Mark II Headphones",
+  desc: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+};
+
+export default async function getHeroData() {
+  return heroData;
+}
