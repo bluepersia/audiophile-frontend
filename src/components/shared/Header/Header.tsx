713b65936a7cf0143f8bd65d5b730909e8f2474d
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-25 12:13:50 -0600

    Header TSX: logo

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,12 +1,13 @@
 import styles from "./header.module.scss";
 import mobileMenuBtn from "../../../assets/shared/mobileMenuBtn.png";
-
+import logo from "../../../assets/shared/logo.png";
 export default function Header() {
   return (
     <header className={styles.header}>
       <button className={styles.menuBtn} aria-label="Toggle mobile menu">
         <img className={styles.menuIcon} alt="" src={mobileMenuBtn} />
       </button>
+      <img className={styles.logo} alt="Audiophile" src={logo} />
     </header>
   );
 }
